877a80481cf050fa1d2f615f23f381cb105a8a13
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-02-25 12:44:42 +0530

    Banner Image compleyted

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
+import {API_KEY,imageUrl } from '../../constants/constants'
+import axios from '../../axios'
 import './Banner.css'
 function Banner() {
+    const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}`).then((response)=>{
+      console.log(response.data.results[Math.floor(Math.random() * 20)])
+      setMovie(response.data.results[Math.floor(Math.random() * 20)])
+    })
+   
+  }, [])
+  
   return (
-    <div className='banner'>
+    <div 
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path: ""})`}}
+      className='banner'>
         <div className='content'>
-          <h1 className='title'>Movie Name</h1>
+          <h1 className='title'>{movie ? movie.title: ""}</h1>
           <div className="banner_buttons">
             <button className='button'>Play</button>
             <button className='button'>My list</button>
           </div>
-          <h1 className="discription">Lorem Picsum The Lorem Ipsum for photos.Easy to use stylish placeholders Specific Image</h1>
+          <h1 className="discription">{movie ? movie.overview : ""}</h1>
         </div>
         <div className='fade_bottom'>
 
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '9d972b854af8445b6d86d5524241c854'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
