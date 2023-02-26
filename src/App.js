8d8146c003ec238a4baf5d538a76aec94ee87295
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-02-26 11:19:38 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import {originals,trending,action,comedy,horror,romance,documentary} from './urls'
 import './App.css'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
@@ -9,7 +10,13 @@ function App(){
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals' />
+      <RowPost url={trending} title='Trending' isSmall />
+      <RowPost url={action} title='Action'  />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={horror} title='Horror'  />
+      <RowPost url={romance} title='Romance' isSmall />
+      <RowPost url={documentary} title='Documentary'  />
     </div>
   );
 }
